5a482b647f115688388893da188b9b71b64d62f1
Mahnoor19 <manomajid19@gmail.com>
2025-05-18 16:10:16 +0500

    Update test_model.py

--- src/tests/test_model.py
+++ src/tests/test_model.py
@@ -1,4 +1,5 @@
-from src.train import train_model
+from train import train_model  # new
+from preprocess import load_data, preprocess_data  # new
 
 def test_model_accuracy():
     accuracy = train_model()
